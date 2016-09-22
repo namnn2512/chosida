31b47cd175fdbc1de82d4cfa5fa5fc237255c375
nam nguyen <nam.k15cmu@gmail.com>
2016-09-22 09:23:43 +0700

    Update production.js

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -19,6 +19,6 @@ module.exports = {
     uri: process.env.MONGODB_URI
       || process.env.MONGOHQ_URL
       || process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME
-      || 'mongodb://localhost/fullstack'
+      || 'mongodb://chosidauser:Abcde12345-@ds035766.mlab.com:35766/chosidadb'
   }
 };
