c7932150d1c8d3190e39ba3e1984b1b2c748b8de
nam nguyen <nam.k15cmu@gmail.com>
2016-09-22 09:22:29 +0700

    Update development.js

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -7,7 +7,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: '  mongodb://chosidauser:Abcde12345-@ds035766.mlab.com:35766/chosidadb'
+    uri: 'mongodb://chosidauser:Abcde12345-@ds035766.mlab.com:35766/chosidadb'
   },
 
   // Seed database on startup
